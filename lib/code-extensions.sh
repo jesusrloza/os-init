7d3ed4b9a9d57b6294eaed90dbdbd0008a3fe88b
Jesús R. Loza <jesusr.loza@gmail.com>
2023-10-18 17:07:41 -0600

    fix: node installation workflow

--- lib/code-extensions.sh
+++ lib/code-extensions.sh
@@ -1,55 +1,47 @@
 #!/usr/bin/sh
 
-# install code extensions only if code is already installed
-if ! command -v code-insiders &> /dev/null
-then
-  echo "code-insiders could not be found"
-else
-  echo "code-insiders found"
-  echo "installing extensions..."
+echo "installing code-insiders extensions..."
+code-insiders --install-extension aaron-bond.better-comments
+code-insiders --install-extension alexcvzz.vscode-sqlite
+code-insiders --install-extension bierner.markdown-mermaid
+code-insiders --install-extension bradlc.vscode-tailwindcss
+code-insiders --install-extension christian-kohler.path-intellisense
+code-insiders --install-extension dbaeumer.vscode-eslint
+code-insiders --install-extension dracula-theme.theme-dracula
+code-insiders --install-extension dsznajder.es7-react-js-snippets
+code-insiders --install-extension eamodio.gitlens
+code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension GitHub.copilot
+code-insiders --install-extension GitHub.copilot-chat
+code-insiders --install-extension GitHub.github-vscode-theme
+code-insiders --install-extension github.vscode-github-actions
+code-insiders --install-extension GrapeCity.gc-excelviewer
+code-insiders --install-extension johnpapa.vscode-peacock
+code-insiders --install-extension moalamri.inline-fold
+code-insiders --install-extension ms-azuretools.vscode-docker
+code-insiders --install-extension ms-python.autopep8
+code-insiders --install-extension ms-python.black-formatter
+code-insiders --install-extension ms-python.isort
+code-insiders --install-extension ms-python.python
+code-insiders --install-extension ms-python.vscode-pylance
+code-insiders --install-extension ms-vscode-remote.remote-containers
+code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension oven.bun-vscode
+code-insiders --install-extension PKief.material-icon-theme
+code-insiders --install-extension rangav.vscode-thunder-client
+code-insiders --install-extension streetsidesoftware.code-spell-checker
+code-insiders --install-extension streetsidesoftware.code-spell-checker-spanish
+code-insiders --install-extension svelte.svelte-vscode
+code-insiders --install-extension tomoki1207.pdf
+code-insiders --install-extension unifiedjs.vscode-mdx
+code-insiders --install-extension usernamehw.errorlens
+code-insiders --install-extension wix.glean
+code-insiders --install-extension wmaurer.change-case
+code-insiders --install-extension yummygum.city-lights-theme
+code-insiders --install-extension zhuangtongfa.material-theme
+code-insiders --install-extension ZixuanChen.vitest-explorer
 
-  code-insiders --install-extension aaron-bond.better-comments
-  code-insiders --install-extension alexcvzz.vscode-sqlite
-  code-insiders --install-extension bierner.markdown-mermaid
-  code-insiders --install-extension bradlc.vscode-tailwindcss
-  code-insiders --install-extension christian-kohler.path-intellisense
-  code-insiders --install-extension dbaeumer.vscode-eslint
-  code-insiders --install-extension dracula-theme.theme-dracula
-  code-insiders --install-extension dsznajder.es7-react-js-snippets
-  code-insiders --install-extension eamodio.gitlens
-  code-insiders --install-extension esbenp.prettier-vscode
-  code-insiders --install-extension GitHub.copilot
-  code-insiders --install-extension GitHub.copilot-chat
-  code-insiders --install-extension GitHub.github-vscode-theme
-  code-insiders --install-extension github.vscode-github-actions
-  code-insiders --install-extension GrapeCity.gc-excelviewer
-  code-insiders --install-extension johnpapa.vscode-peacock
-  code-insiders --install-extension moalamri.inline-fold
-  code-insiders --install-extension ms-azuretools.vscode-docker
-  code-insiders --install-extension ms-python.autopep8
-  code-insiders --install-extension ms-python.black-formatter
-  code-insiders --install-extension ms-python.isort
-  code-insiders --install-extension ms-python.python
-  code-insiders --install-extension ms-python.vscode-pylance
-  code-insiders --install-extension ms-vscode-remote.remote-containers
-  code-insiders --install-extension ms-vsliveshare.vsliveshare
-  code-insiders --install-extension oven.bun-vscode
-  code-insiders --install-extension PKief.material-icon-theme
-  code-insiders --install-extension rangav.vscode-thunder-client
-  code-insiders --install-extension streetsidesoftware.code-spell-checker
-  code-insiders --install-extension streetsidesoftware.code-spell-checker-spanish
-  code-insiders --install-extension svelte.svelte-vscode
-  code-insiders --install-extension tomoki1207.pdf
-  code-insiders --install-extension unifiedjs.vscode-mdx
-  code-insiders --install-extension usernamehw.errorlens
-  code-insiders --install-extension wix.glean
-  code-insiders --install-extension wmaurer.change-case
-  code-insiders --install-extension yummygum.city-lights-theme
-  code-insiders --install-extension zhuangtongfa.material-theme
-  code-insiders --install-extension ZixuanChen.vitest-explorer
-  
-  echo "copying settings..."
-  mkdir -p ~/.config/Code\ -\ Insiders/User
-  cat ./config/code_settings.json > ~/.config/Code\ -\ Insiders/User/settings.json
-  cat ./config/code_keybindings.json > ~/.config/Code\ -\ Insiders/User/keybindings.json
-fi
+echo "setting user configurations..."
+mkdir -p ~/.config/Code\ -\ Insiders/User
+cat ./config/code_settings.json > ~/.config/Code\ -\ Insiders/User/settings.json
+cat ./config/code_keybindings.json > ~/.config/Code\ -\ Insiders/User/keybindings.json
--- lib/node.sh
+++ lib/node.sh
@@ -1,39 +1,29 @@
 #!/usr/bin/sh
 
 # installing nvm and latest node lts (https://github.com/nvm-sh/nvm)
-  if ! command -v nvm &> /dev/null
-  then
-    echo "nvm could not be found"
-    echo "installing nvm..."
-
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-    source ~/.bashrc
-
-    echo "installing latest nodejs lts..."
-    nvm install --lts
-    nvm use --lts
-    nvm alias default lts/*
-  fi
-
-# install pnpm (https://pnpm.io/)
-  if ! command -v pnpm &> /dev/null
-  then
-    echo "pnpm could not be found"
-    echo "installing pnpm..."
-    npm install -g pnpm
-
-    echo "configuring pnpm..."
-    pnpm setup
-    source ~/.bashrc
-
-    pnpm add -g tldr         # TLDR pages (https://github.com/tldr-pages/tldr)
-    pnpm add -g lite-server
-  fi
+  echo "installing nvm..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+  
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  
+  echo "installing latest nodejs lts..."
+  nvm install --lts
+  nvm use --lts
+  nvm alias default lts/*
 
 # install bun (https://bun.sh/)
-if ! command -v bun &> /dev/null
-then
-  echo "bun could not be found"
   echo "installing bun..."
   curl -fsSL https://bun.sh/install | bash
-fi
+
+
+# install pnpm (https://pnpm.io/)
+  echo "installing pnpm..."
+  npm install -g pnpm
+  pnpm setup
+  
+  # pnpm add -g pnpm 
+  # pnpm add -g tldr         # TLDR pages (https://github.com/tldr-pages/tldr)
+  # pnpm add -g lite-server
+
